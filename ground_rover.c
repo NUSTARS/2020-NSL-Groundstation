98a319594c0a74d279bbbdc01e5dc2a0c3d9c85e
kkuzminskas <kendallkuzminskas2020@u.northwestern.edu>
2020-01-29 11:50:12 -0600

    generic functions

--- ground_rover.c
+++ ground_rover.c
@@ -1,1 +1,43 @@
 
+struct go_car{
+   //make bytes for each motor (4 total)
+  // potentially one byte for checksum
+  std::byte forward_back;
+  std::byte turn_wheel;
+  std::byte lift_ice_1;
+  std::byte lift_ice_2;
+  std::byte checksum;
+};
+
+
+void keyboard_io(){
+ //accepts input/output from keyboard
+  // ex: if hit up arrow, direction should be forward
+ //if super long make translation from io into data its own function 
+  
+  //nathan
+}
+
+
+
+void send_data_radio(){
+ //send the data to the serial radio
+  //optionally create a checksum
+  //kendall
+}
+
+
+void send_data_rover(){
+ //see foundation code from last year
+  // might not be in c++
+  //sends data from radio to car
+ //kendall 
+}
+
+void check_checksum(){
+ //checks checksum from the ack message from rover 
+  
+  //kendall
+}
+
+
